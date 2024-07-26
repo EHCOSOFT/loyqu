4aec5ceaaa58e2f18f29621d302a9e16ba3413dd
Scarlettszsz <125333428+Scarlettszsz@users.noreply.github.com>
2024-07-26 09:44:25 +0900

    20240726 수정사항

--- resources/assets/js/common.js
+++ resources/assets/js/common.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     // 공통 jQuery 코드 (모바일과 PC 모두에 적용)
     function applyCommonjQuery() {
         $("#header").append(Header());
-        
+
         function Header() {
             return `
             <div class="container">
@@ -64,6 +64,51 @@ $(document).ready(function () {
             var tabId = $(this).attr('href');
             $('.tab-content').removeClass('active');
             $(tabId).addClass('active');
+        });
+
+        // header dropdown event 
+        // .dropdown을 클릭할 때 active 클래스를 토글
+        $('.dropdown > a').click(function (event) {
+            event.stopPropagation();
+            $('.dropdown-list').toggleClass('active');
+        });
+
+        // 검색 버튼 클릭 시
+        // .btn-search 버튼 클릭 시
+        $('.btn-search').click(function () {
+            var searchText = $('#addressSearch').val().trim();
+
+            // 예시
+            // "해운대"를 검색한 경우
+            if (searchText === '해운대') {
+                $('.search-list ul').html('<li><b>검색결과</b> 해운대</li>');
+                $('.search-list').addClass('active');
+            } else {
+                // 검색어가 "해운대"가 아닌 경우
+                $('.search-list ul').html('<li>검색 결과가 없습니다</li>');
+                $('.search-list').addClass('active');
+            }
+        });
+
+        // 입력 필드에서 엔터 키를 눌렀을 때도 검색을 실행
+        $('#addressSearch').keypress(function (event) {
+            if (event.which === 13) { // Enter 키의 키코드는 13
+                $('.btn-search').click();
+            }
+        });
+
+        // 입력 필드에서 엔터 키를 눌렀을 때도 검색을 실행
+        $('#addressSearch').keypress(function (event) {
+            if (event.which === 13) { // Enter 키의 키코드는 13
+                $('.btn-search').click();
+            }
+        });
+
+        // .dropdown-list 외부를 클릭할 때 active 클래스를 제거
+        $(document).click(function (event) {
+            if (!$(event.target).closest('.dropdown').length) {
+                $('.dropdown-list').removeClass('active');
+            }
         });
 
         // detailSwiper
